0237c4e8d9faed81e47c4c9cdd1dfcfd5bbfeb0e
Woo Hyung Choi <whchoi98@gmail.com>
2020-03-02 02:24:36 +0900

    Edit - New VPC Template add

--- pingshell.sh
+++ pingshell.sh
@@ -4,8 +4,8 @@ cat ./server.txt |  while read output
 do
     ping -c 3 "$output" > /dev/null
     if [ $? -eq 0 ]; then
-    echo "node $output is up" 
+    echo "SERVER $output is up" 
     else
-    echo "node $output is down"
+    echo "SERVER $output is down"
     fi
 done
